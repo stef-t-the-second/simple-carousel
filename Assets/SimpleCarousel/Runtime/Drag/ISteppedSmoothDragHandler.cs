d3ca6a5da9d9a0c4ab51a4f8cf60221512349446
steft <52441940+stef-t@users.noreply.github.com>
2025-04-21 23:40:09 +0200

    removing todo

--- Assets/SimpleCarousel/Runtime/Drag/ISteppedSmoothDragHandler.cs
+++ Assets/SimpleCarousel/Runtime/Drag/ISteppedSmoothDragHandler.cs
@@ -3,8 +3,6 @@ using UnityEngine.EventSystems;
 
 namespace Steft.SimpleCarousel.Drag
 {
-    // TODO this needs to be refactored into a sort of "ISteppedSmoothDragHandler"
-    //      parts of "SimpleCarouselView.Update" needs to migrated to "NormalizedDrag.Update"
     public interface ISteppedSmoothDragHandler : IBeginDragHandler, IEndDragHandler, IDragHandler
     {
         void Init(float startScrollIndex, float maximumScrollIndex);
