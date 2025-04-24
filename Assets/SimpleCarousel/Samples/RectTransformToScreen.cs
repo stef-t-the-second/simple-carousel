18ffa250d4f306b2602776ad00fc8c4ca72cebb3
steft <52441940+stef-t@users.noreply.github.com>
2025-04-24 23:29:26 +0200

    enabling 3d effect for carousel by setting the Canvas up in "Render Mode" "World Space"

---
+++ Assets/SimpleCarousel/Samples/RectTransformToScreen.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace Steft.SimpleCarousel.Samples
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(RectTransform))]
+    public class RectTransformToScreen : MonoBehaviour
+    {
+        private RectTransform m_RectTransform;
+
+        private void Awake()
+        {
+            m_RectTransform = transform as RectTransform;
+            if (m_RectTransform == null)
+                throw new NullReferenceException();
+
+            m_RectTransform.ResetToMiddleCenter();
+            m_RectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            m_RectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+        }
+#endif
+    }
+}
