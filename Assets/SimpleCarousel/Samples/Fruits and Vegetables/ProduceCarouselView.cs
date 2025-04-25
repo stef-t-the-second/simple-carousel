3af924b52d8c593f72940d76547794717a9a7f5d
steft <52441940+stef-t@users.noreply.github.com>
2025-04-25 22:16:56 +0200

    implementing context menu methods to test "CenterNext" and "CenterPrevious"

--- Assets/SimpleCarousel/Samples/Fruits and Vegetables/ProduceCarouselView.cs
+++ Assets/SimpleCarousel/Samples/Fruits and Vegetables/ProduceCarouselView.cs
@@ -1,9 +1,19 @@
 using Steft.SimpleCarousel;
 using Steft.SimpleCarousel.Samples;
+using UnityEngine;
 
 namespace SimpleCarousel.Samples
 {
     internal class ProduceCarouselView : CarouselView<ProduceData>
     {
+#region In-Editor Testing
+
+        [ContextMenu(nameof(InvokeCenterNext))]
+        private void InvokeCenterNext() => CenterNext(true);
+
+        [ContextMenu(nameof(InvokeCenterPrevious))]
+        private void InvokeCenterPrevious() => CenterPrevious(true);
+
+#endregion
     }
 }
